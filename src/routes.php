3f050fac3cae84ea98b975475266d7b50018f254
Anton Tibblin <tibbelit@gmail.com>
2016-03-29 23:30:24 +0200

    Updates

--- src/routes.php
+++ src/routes.php
@@ -30,6 +30,15 @@ $app->get('/today', function ($request, $response, $args) {
 /*
     Routes to different sources of menus
 */
+$app->get('/restaurants', function ($request, $response, $args) {
+    $restaurants = [];
+    $restaurants["Restaurants"] = Restaurant::restaurants();
+    $response = $response->withHeader('Access-Control-Allow-Origin', '*');
+    $response = $response->withStatus(200);
+    $response = $response->withJson($restaurants);
+    return $response;
+});
+
 $app->get('/{restaurant}', function ($request, $response, $args) {
     $restaurantName = $args["restaurant"];
     if(class_exists($restaurantName)){
