36e32dc1f3df2ee4f41e8a8e1b45de1adaba5d47
Anton Tibblin <tibbelit@gmail.com>
2016-04-01 00:43:34 +0200

    Added slack webbhook
    To tips for lunch every day! =)

--- src/routes.php
+++ src/routes.php
@@ -28,6 +28,49 @@ $app->get('/today', function ($request, $response, $args) {
 });
 
 /*
+	Slack
+*/
+$app->get('/slack', function ($request, $response, $args) {
+	$restaurants = Restaurant::restaurants();
+    $att = new ArrayObject();
+    foreach($restaurants as $restaurant){
+		$rJson = new ArrayObject();
+        $r = new $restaurant();
+		$rJson["title"] = "$r->name";
+		$food = "";
+		foreach(json_decode($r->getDayMenu()) as $category => $dish){
+			$food .= "*$category*: $dish->title\n";
+		}
+		$rJson["text"] = $food;
+		$rJson["fallback"] = $food;
+		$rJson["color"] = "#36a64f";
+		$rJson["mrkdwn_in"] = ["text", "pretext"];
+		$att[] = $rJson;
+    }
+	
+	$url = 'https://hooks.slack.com/services/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';
+	$json = new ArrayObject();
+	$json["text"] = "Dagens lunch presenteras genom API: <http://mahlunch.antontibblin.se|MAHLunch> (<https://github.com/Tibbelit/MAH-Lunch-API|Github>), mer info hittar ni på <http://niagaralunch.antontibblin.se|Webbappen: NiagaraLunch> (<https://github.com/Tibbelit/MAH-Lunch-Webapp|Github>)";
+	$json["username"] = "Lunchdags";
+	$json["icon_emoji"] = ":hamburger:";
+	$json["channel"] = "Antons";
+	$json["mrkdwn"] = true;
+	$json["attachments"] = $att;
+	$data = array('payload' => json_encode($json));
+
+	// use key 'http' even if you send the request to https://...
+	$options = array(
+		'http' => array(
+			'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+			'method'  => 'POST',
+			'content' => http_build_query($data)
+		)
+	);
+	$context  = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+});
+
+/*
     Routes to different sources of menus
 */
 $app->get('/restaurants', function ($request, $response, $args) {
